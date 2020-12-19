276de0f69dd3a89d7db7c4b32e01fa661f378a52
Serpash <ser7290@gmail.com>
2020-12-19 20:20:39 +0300

    ADDED: 2 functions and 2 radiobuttons

--- src/bsu/serega/pashko/sevengroup/MainFrame.java
+++ src/bsu/serega/pashko/sevengroup/MainFrame.java
@@ -19,3 +19,57 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
+
+public class MainFrame extends JFrame {
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 320;
+    private JTextField textFieldX;
+    private JTextField textFieldY;
+    private JTextField textFieldZ;
+    private JTextField textFieldMem;
+    private JTextField textFieldMem2;
+    private JTextField textFieldMem3;
+// Текстовое поле для отображения результата,
+
+    // как компонент, совместно используемый в различных методах
+    private JTextField textFieldResult;
+    // Группа радио-кнопок для обеспечения уникальности выделения в группе
+    private ButtonGroup radioButtons = new ButtonGroup();
+    private ButtonGroup radioMemoryButtons = new ButtonGroup();
+    // Контейнер для отображения радио-кнопок
+    private Box hboxFormulaType = Box.createHorizontalBox();
+    private Box hboxMemoryType = Box.createHorizontalBox();
+    // Идентификатор выбранной формулы
+    private int formulaId = 1;
+    private int memoryId = 1;
+
+    public Double calculate1(Double x, Double y, Double z) {
+        return Math.pow(Math.cos(Math.PI * Math.pow(x, 3)) + Math.log(Math.pow(1 + y, 2)), 1 / 4) * (Math.exp(z * z) + Math.sqrt(1 / x) + Math.cos(Math.exp(y)));
+    }
+
+    // Формула №2 для рассчѐта
+    public Double calculate2(Double x, Double y, Double z) {
+        return Math.exp(0.5 * x) / (Math.sqrt(z + y) * Math.log(Math.pow(x, z)));
+    }
+
+    // Вспомогательный метод для добавления кнопок на панель
+    private void addRadioButton(String buttonName, final int formulaId) {
+        JRadioButton button = new JRadioButton(buttonName);
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+                MainFrame.this.formulaId = formulaId;
+            }
+        });
+        radioButtons.add(button);
+        hboxFormulaType.add(button);
+    }
+    private void addRadioButton2(String buttonName, final int memoryId) {
+        JRadioButton button = new JRadioButton(buttonName);
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+                MainFrame.this.memoryId = memoryId;
+            }
+        });
+        radioMemoryButtons.add(button);
+        hboxMemoryType.add(button);
+    }
